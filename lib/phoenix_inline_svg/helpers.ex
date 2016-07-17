8e0ea7fad323e3f8df8322352402058a826d3783
Nikko Miu <nikko.miu@nikkomiu.com>
2016-07-17 15:31:05 -0500

    Fixed issue with unsafe string

--- lib/phoenix_inline_svg/helpers.ex
+++ lib/phoenix_inline_svg/helpers.ex
@@ -59,13 +59,15 @@ defmodule PhoenixInlineSvg.Helpers do
 
   """
   def svg_image(conn, name, collection \\ nil) do
-    if collection == nil do
-      collection = config_or_default(:default_collection, "generic")
-    end
+    coll =
+      case collection do
+        nil -> config_or_default(:default_collection, "generic")
+        c -> c
+      end
 
-    "#{collection}/#{name}.svg"
+    "#{coll}/#{name}.svg"
     |> read_svg_file(conn)
-    |> wrap_svg(collection, name)
+    |> wrap_svg(coll, name)
     |> safety_string()
   end
 
