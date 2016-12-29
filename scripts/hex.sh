3710f1d8aedd1469cc5e68534075adaac202f521
Nikko Miu <nikko.miu@nikkomiu.com>
2016-12-29 12:52:02 -0600

    Updated Hex.sh to use correct commands

--- scripts/hex.sh
+++ scripts/hex.sh
@@ -2,17 +2,13 @@
 
 ## Setup user
 mkdir -p ~/.hex
-echo '{username,<<"'${HEX_USERNAME}'">>}' > ~/.hex/hex.config
-echo '{encrypted_key,<<"'${HEX_KEY}'">>}' > ~/.hex/hex.config
+echo '{username,<<"'${HEX_USERNAME}'">>}.' > ~/.hex/hex.config
+echo '{encrypted_key,<<"'${HEX_KEY}'">>}.' >> ~/.hex/hex.config
 
-## Add rebar3 to global
-mkdir -p ~/.config/rebar3
-echo '{plugins, [rebar3_hex]}.' > ~/.config/rebar3/rebar.config
-
-mix hex publish <<EOF
+mix hex.publish <<EOF
 y
 EOF
 
-mix hex publish.docs <<EOF
+mix hex.publish docs <<EOF
 y
 EOF
