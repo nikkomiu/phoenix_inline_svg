8a6cc2286873b6ef89de99a124a9e94f830d1aad
Nikko Miu <nikkoamiu@gmail.com>
2019-04-08 10:39:28 -0500

    Updated doc packages

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PhoenixInlineSvg.Mixfile do
 
   def project do
     [app: :phoenix_inline_svg,
-     version: "1.1.2",
+     version: "1.2.0",
      elixir: "~> 1.3",
      deps: deps(),
      package: package(),
@@ -25,10 +25,10 @@ defmodule PhoenixInlineSvg.Mixfile do
 
   defp deps do
     [{:phoenix, "~> 1.2"},
-     {:inch_ex, "~> 0.5", only: [:dev, :test]},
+     {:inch_ex, "~> 1.0", only: [:dev, :test]},
      {:credo, "~> 0.5", only: [:dev, :test]},
      {:excoveralls, "~> 0.5", only: [:dev, :test]},
-     {:ex_doc, ">= 0.0.0", only: [:dev, :test]}]
+     {:ex_doc, ">= 0.20.0", only: [:dev, :test], runtime: false}]
   end
 
   defp description do
