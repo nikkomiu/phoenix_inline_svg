4b75f15f6b4667a264a13bdd0c0db217d41f9ef7
Mitchell Hanberg <mitch@mitchellhanberg.com>
2019-05-16 21:09:52 -0400

    Update credo version and travis config

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule PhoenixInlineSvg.Mixfile do
   def project do
     [
       app: :phoenix_inline_svg,
-      version: "1.2.0",
-      elixir: "~> 1.3",
+      version: "1.2.1",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
       package: package(),
@@ -34,7 +34,7 @@ defmodule PhoenixInlineSvg.Mixfile do
       {:phoenix, "~> 1.4"},
       {:floki, "~> 0.21.0"},
       {:inch_ex, "~> 1.0", only: [:dev, :test]},
-      {:credo, "~> 0.5", only: [:dev, :test]},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.5", only: [:dev, :test]},
       {:ex_doc, ">= 0.20.0", only: [:dev], runtime: false}
     ]
