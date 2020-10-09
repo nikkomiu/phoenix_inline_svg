4bf986a1c4a9bec866e53f469012b7d338b771c8
dvic <info@dvic.io>
2020-10-09 19:15:44 +0200

    run mix format

--- test/svg_icon_test.exs
+++ test/svg_icon_test.exs
@@ -2,8 +2,8 @@ defmodule PhoenixInlineSvgTest do
   use ExUnit.Case
   doctest PhoenixInlineSvg
 
-#  test "showing an svg"
-#  test "showing error when not found"
-#  test "config dir variable"
-#  test "config not found variable"
+  #  test "showing an svg"
+  #  test "showing error when not found"
+  #  test "config dir variable"
+  #  test "config not found variable"
 end
